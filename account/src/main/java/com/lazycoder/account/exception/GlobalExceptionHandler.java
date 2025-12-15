aa53e0d3393bd5818320ba4bf6029de5ae8215ea
abhishekdeepak02 <yadu.akd@gmail.com>
2025-12-15 09:56:32 +0530

    Handle validation error in controller response

--- account/src/main/java/com/lazycoder/account/exception/GlobalExceptionHandler.java
+++ account/src/main/java/com/lazycoder/account/exception/GlobalExceptionHandler.java
@@ -1,16 +1,42 @@
 package com.lazycoder.account.exception;
 
 import com.lazycoder.account.dto.ErrorResponseDto;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected @Nullable ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                            HttpHeaders headers,
+                                                                            HttpStatusCode status,
+                                                                            WebRequest request) {
+
+
+        Map<String, String> validationErrors = new HashMap<>();
+        List<ObjectError> valicationErrorList = ex.getBindingResult().getAllErrors();
+        valicationErrorList.forEach((error) -> {
+            String fieldName = ((org.springframework.validation.FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            validationErrors.put(fieldName, message);
+        });
+        return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalException(Exception exception,
