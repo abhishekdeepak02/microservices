83aea6b85c154c1dd14d06f954d9cb342de6e8a0
abhishekdeepak02 <yadu.akd@gmail.com>
2025-12-15 11:16:44 +0530

    Documenting REST API using @OpenAPIDefinition

--- account/src/main/java/com/lazycoder/account/AccountApplication.java
+++ account/src/main/java/com/lazycoder/account/AccountApplication.java
@@ -1,11 +1,30 @@
 package com.lazycoder.account;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(title = "Accounts Microservice REST API documentation",
+        description = "LazyCoder Accounts Microservice REST API documentation",
+        version = "v1",
+        contact = @Contact (
+                name = "Abhishek",
+                email = "learning@lazycoder.com",
+                url = "http://www.lazycoder.com")
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "LazyCoder Accounts Microservice Wiki Documentation",
+                url = "http://www.lazycoder.com/wiki"
+        )
+)
 public class AccountApplication {
 
 	public static void main(String[] args) {
