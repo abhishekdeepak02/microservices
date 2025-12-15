9ebcd2e97118c25d15da028bd864b6d7c6f61e6e
abhishekdeepak02 <yadu.akd@gmail.com>
2025-12-15 11:39:40 +0530

    Enhanced the REST API documentation using @Tag @Operation @ApiResponse

--- account/src/main/java/com/lazycoder/account/controller/AccountsController.java
+++ account/src/main/java/com/lazycoder/account/controller/AccountsController.java
@@ -4,6 +4,10 @@ import com.lazycoder.account.constants.AccountsConstants;
 import com.lazycoder.account.dto.CustomerDto;
 import com.lazycoder.account.dto.ResponseDto;
 import com.lazycoder.account.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -13,6 +17,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Accounts REST APIs for CRUD operations",
+        description = "CRUD REST APIs for Lazy bank to CREATE, UPDATE, FETCH and DELETE account information"
+)
 @RestController
 @RequestMapping(path = "/api/account", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
@@ -21,6 +29,14 @@ public class AccountsController {
 
     private IAccountsService iAccountsService;
 
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create account for a customer in Lazy Bank"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 
@@ -30,6 +46,14 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Fetch Account REST API",
+            description = "REST API to fetch account info for a account holder in Lazy Bank."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccount(@RequestParam
             @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digit.")
@@ -41,6 +65,20 @@ public class AccountsController {
                 .body(customerDto);
     }
 
+    @Operation(
+            summary = "Update Account REST API",
+            description = "REST API to update account info for a existing account holder in Lazy Bank."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL SERVER ERROR"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
 
@@ -54,6 +92,20 @@ public class AccountsController {
         }
     }
 
+    @Operation(
+            summary = "DELETE Account REST API",
+            description = "REST API to delete account info for a existing account holder in Lazy Bank."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL SERVER ERROR"
+            )
+    })
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountInfo(@RequestParam
             @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digit.")
